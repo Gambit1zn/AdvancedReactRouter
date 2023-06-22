2b9152cb6ea6b105a8fbc00c6f8bf853c0db5dd0
Ethan Ankadu <ethanankadu1zn@gmail.com>
2023-06-22 21:24:48 +0200

    completed project

--- frontend/src/pages/RootLayout.js
+++ frontend/src/pages/RootLayout.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+
+import MainNavigation from '../components/MainNavigation';
 
 const RootLayout = () => {
   return (
-    <React.Fragment>
-
-    </React.Fragment>
+    <>
+    <MainNavigation>
+        <main>
+          <Outlet/>
+        </main>
+      </MainNavigation>
+    </>
   );
 }
 
