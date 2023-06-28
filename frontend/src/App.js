c6b95666ec0f0800153cd106c9aa742b92003436
Ethan Ankadu <ethanankadu1zn@gmail.com>
2023-06-28 21:55:20 +0200

    used react router loader to prefetch data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -27,26 +27,45 @@
 // DONE
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 // DONE
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-import EditEventPage from './pages/EditEvent';
-import EventDetailPage from './pages/EventDetail';
-import EventsPage from './pages/Events';
-import EventsRootLayout from './pages/EventsRoot';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
-import RootLayout from './pages/Root';
+import EditEventPage from "./pages/EditEvent";
+import EventDetailPage from "./pages/EventDetail";
+import EventsPage from "./pages/Events";
+import EventsRootLayout from "./pages/EventsRoot";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <RootLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      {path: 'events', element: <EventsPage/>},
-      { path: 'events/:eventId', element: <EventDetailPage /> },
-      { path: 'new', element: <NewEventPage /> },
-      { path: ':eventId/edit', element: <EditEventPage /> },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          { 
+            index: true, 
+            element: <EventsPage />, 
+            loader: async () => {
+              const response = await fetch('http://localhost:8080/events');
+
+              if (!response.ok) {
+               //..
+              } else {
+                const resData =  response.json();
+                return resData.events;
+              }
+            } 
+          },
+          { path: ":eventId", element: <EventDetailPage /> },
+          { path: "new", element: <NewEventPage /> },
+          { path: ":eventId/edit", element: <EditEventPage /> },
+        ],
+      },
     ],
   },
 ]);
