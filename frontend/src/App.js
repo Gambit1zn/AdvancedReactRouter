2e7aaa67c9f63d985170db5f67b51d56c852e952
Ethan Ankadu <ethanankadu1zn@gmail.com>
2023-10-08 19:11:07 +0200

    Completed delete functionality using another action but this time it is triggered programmatically

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,11 +30,14 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import EditEventPage from "./pages/EditEvent";
-import EventDetailPage, {loader as eventDetailLoader} from "./pages/EventDetail";
-import EventsPage, {loader as eventsLoader} from "./pages/Events";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from "./pages/EventDetail";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
-import NewEventPage, {action as newEventAction} from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
 
@@ -42,28 +45,28 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
-    errorElement: <ErrorPage/>,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       {
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { 
-            index: true, 
-            element: <EventsPage />, 
-            loader: eventsLoader
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
           },
           {
-            path: ':eventId',
-            id: 'event-detail',
+            path: ":eventId",
+            id: "event-detail",
             loader: eventDetailLoader,
             children: [
-              { index: true, element: <EventDetailPage />},
-              { path: ':edit', element: <EditEventPage /> }, 
-            ]
+              { index: true, element: <EventDetailPage />, action: deleteEventAction },
+              { path: ":edit", element: <EditEventPage /> },
+            ],
           },
-          { path: 'new', element: <NewEventPage />, action: newEventAction },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
